d7cc56fbbfccd27bd52d687fa3e234ff1fcfba3f
Ignacio <ignacio.magana@me.com>
2018-10-08 04:44:14 -0700

    3d_kde in place (needs to implement lum weighting using gal.m)

--- gwcosmo/master.py
+++ gwcosmo/master.py
@@ -42,7 +42,7 @@ class MasterEquation(object):
         # Also note, when zmax is set too high (ie 6.0), it can cause px_H0nG to incorrectly evaluate to 0 for some values of H0.
         self.zmax = 1.0 # TODO: change so that this is set by some property of pdet
 
-    def px_H0G(self,event_data,skymap2d=None):
+    def px_H0G(self,event_data,skymap2d=None,use_3d_kde=True):
         """
         The likelihood of the GW data given the source is in the catalogue and given H0 (will eventually include luminosity weighting). 
         
@@ -57,26 +57,32 @@ class MasterEquation(object):
         skykernel = event_data.compute_2d_kde()
         distkernel = event_data.lineofsight_distance()
 
-        num = np.zeros(len(self.H0)) 
-        # loop over all possible galaxies
-        for i in range(nGal):
-            gal = self.galaxy_catalog.get_galaxy(i)
-            
-            if self.weighted:
-                weight = L_mdl(gal.m,dl_zH0(gal.z,self.H0)) # TODO: make this compatible with all galaxy catalogs (ie make gal.m universal)
-            else:
-                weight = 1.0
-            
-            # TODO: add possibility of using skymaps/other ways of using gw data
-            if skymap2d is not None:
-                tempsky = skymap2d.skyprob(gal.ra,gal.dec) # TODO: test fully and integrate into px_H0nG
-            else:
-                tempsky = skykernel.evaluate([gal.ra,gal.dec])*4.0*np.pi/np.cos(gal.dec) # remove uniform sky prior from samples
-            
-            tempdist = distkernel(dl_zH0(gal.z,self.H0,linear=self.linear))/dl_zH0(gal.z,self.H0,linear=self.linear)**2 # remove dl^2 prior from samples
-            
-            num += tempdist*tempsky*weight
+        num = np.zeros(len(self.H0))
+        
+        if use_3d_kde == True:
+            ra, dec, dist, z, lumB = self.extract_galaxies()
+            for k, x in enumerate(self.H0):
+                coverh = (const.c.to('km/s') / (x * u.km / u.s / u.Mpc)).value
+                num[k] = event_data.compute_3d_probability(ra, dec, dist, z, lumB, coverh) # TODO: lumB does the weighting here in the "trivial" way...
 
+        else: # loop over all possible galaxies
+            for i in range(nGal):
+                gal = self.galaxy_catalog.get_galaxy(i)
+
+                if self.weighted:
+                    weight = L_mdl(gal.m,dl_zH0(gal.z,self.H0)) # TODO: make this compatible with all galaxy catalogs (ie make gal.m universal)
+                else:
+                    weight = 1.0
+
+                # TODO: add possibility of using skymaps/other ways of using gw data
+                if skymap2d is not None:
+                    tempsky = skymap2d.skyprob(gal.ra,gal.dec) # TODO: test fully and integrate into px_H0nG
+                else:
+                    tempsky = skykernel.evaluate([gal.ra,gal.dec])*4.0*np.pi/np.cos(gal.dec) # remove uniform sky prior from samples
+
+                tempdist = distkernel(dl_zH0(gal.z,self.H0,linear=self.linear))/dl_zH0(gal.z,self.H0,linear=self.linear)**2 # remove dl^2 prior from samples
+
+                num += tempdist*tempsky*weight
         return num
 
 
@@ -231,88 +237,53 @@ class MasterEquation(object):
         else:
             return pH0
         
-    def likelihood(self,event_data,complete=False,skymap2d=None):
+    def likelihood(self,event_data,complete=False,skymap2d=None,use_3d_kde=True):
         """
         The likelihood for a single event
         """    
         dH0 = self.H0[1]-self.H0[0]
         
-        pxG = self.px_H0G(event_data,skymap2d)
+        print("calculating pxG")
+        pxG = self.px_H0G(event_data,skymap2d,use_3d_kde)
         if all(self.pDG)==None:
+            print("calculating pDG")
             self.pDG = self.pD_H0G()
         
         if complete==True:
             likelihood = pxG/self.pDG 
         else:
             if all(self.pGD)==None:
-                self.pGD = self.pG_H0D()    
+                print("calculating pGD")
+                self.pGD = self.pG_H0D()
             if all(self.pnGD)==None:
+                print("calculating pnGD")
                 self.pnGD = self.pnG_H0D()
             if all(self.pDnG)==None:
+                print("calculating pDnG")
                 self.pDnG = self.pD_H0nG()
-            
+                
+            print("calculating pxnG")
             pxnG = self.px_H0nG(event_data)
 
             likelihood = self.pGD*(pxG/self.pDG) + self.pnGD*(pxnG/self.pDnG)
             
         return likelihood/np.sum(likelihood)/dH0
     
-    def p_G(self):
-        """
-        The prior probability that a galaxy is in the catalog.
-        """
-        L0 = 1.98e-2 # Kopparapu et al
-        #def I(z):
-        #    return L0*z**2
-        I = lambda x: L0*x**2
-        
+    def extract_galaxies(self):
         nGal = self.galaxy_catalog.nGal()
-        zgal = np.zeros(nGal)
+        ra = np.zeros(nGal)
+        dec = np.zeros(nGal)
+        dist = np.zeros(nGal)
+        z = np.zeros(nGal)
+        lumB = np.zeros(nGal)
         for i in range(nGal):
             gal = self.galaxy_catalog.get_galaxy(i)
-            zgal[i] = gal.z
-        zgal = np.sort(zgal)
-        zs = max(zgal)
-        
-        dl = np.linspace(0.1,400.0,20)
-        zH0 = dl*self.H0/const.c.to('km/s').value
-    
-        zG = np.zeros(len(self.H0))
-        znG = np.zeros(len(self.H0))
-        for i in range(len(self.H0)):
-            if zH0[i] < zs:
-                zG[i] = zH0[i]
-            else: 
-                znG[i] = zH0[i]
-        
-        LG = L0*pz_nG(zG)/quad(I, 0., zs)[0]
-        LnG = L0*pz_nG(znG)/quad(I, zs, self.zmax)[0]
-        return LG/(LG + LnG)
-
-    def p_nG(self):
-        """
-        The prior probability that a galaxy is not in the catalog.
-        
-        Returns the complement of pG.
-        """
-        return 1.0 - self.p_G()
-    
-    def likelihood_PRB(self,event_data):
-        """
-        The likelihood for a single event
-        """ 
-        pG = self.p_G()
-        pnG = self.p_nG()
-        
-        dH0 = self.H0[1]-self.H0[0]
-        
-        #pxG = self.px_H0G(event_data)
-        pxG = self.px_H0G3D(event_data)
-        pxnG = self.px_H0nG(event_data)
-
-        likelihood_prb = pG*pxG + pnG*pxnG
-            
-        return likelihood_prb
+            ra[i] = gal.ra
+            dec[i] = gal.dec
+            dist[i] = gal.distance
+            z[i] = gal.z
+            lumB[i] = gal.lumB
+        return ra, dec, dist, z, lumB
 
 class pofH0(object):
     """
@@ -486,4 +457,72 @@ class pofH0(object):
             tmpd = coverh * x
             tmpp = (3.0*coverh*4.0*np.pi*0.33333*blue_luminosity_density*(tmpd-0.0)**2)
             return ma.masked_where(tmpd<435.,tmpp).filled(0)
+
+class PRB(object):
+    """
+    Class that contains ingredients necessary to compute P(H0) in a PRB way.
+    """
+    def __init__(self,H0,galaxy_catalog,pdet,linear=False,zmax=1.0,dmax=400.0):
+        self.H0 = H0
+        self.galaxy_catalog = galaxy_catalog
+        self.pdet = pdet
+        self.linear = linear
+        self.dmax = dmax
+        self.zmax = zmax        
+
+    def p_G(self):
+        """
+        The prior probability that a galaxy is in the catalog.
+        """
+        L0 = 1.98e-2 # Kopparapu et al
+        #def I(z):
+        #    return L0*z**2
+        I = lambda x: L0*x**2
+        
+        nGal = self.galaxy_catalog.nGal()
+        zgal = np.zeros(nGal)
+        for i in range(nGal):
+            gal = self.galaxy_catalog.get_galaxy(i)
+            zgal[i] = gal.z
+        zgal = np.sort(zgal)
+        zs = max(zgal)
+        
+        dl = np.linspace(0.1,self.dmax,20)
+        zH0 = dl*self.H0/const.c.to('km/s').value
     
+        zG = np.zeros(len(self.H0))
+        znG = np.zeros(len(self.H0))
+        for i in range(len(self.H0)):
+            if zH0[i] < zs:
+                zG[i] = zH0[i]
+            else: 
+                znG[i] = zH0[i]
+        
+        LG = L0*pz_nG(zG)/quad(I, 0., zs)[0]
+        LnG = L0*pz_nG(znG)/quad(I, zs, self.zmax)[0]
+        return LG/(LG + LnG)
+
+    def p_nG(self):
+        """
+        The prior probability that a galaxy is not in the catalog.
+        
+        Returns the complement of pG.
+        """
+        return 1.0 - self.p_G()
+    
+    def likelihood_PRB(self,event_data):
+        """
+        The likelihood for a single event
+        """ 
+        pG = self.p_G()
+        pnG = self.p_nG()
+        
+        dH0 = self.H0[1]-self.H0[0]
+        
+        #pxG = self.px_H0G(event_data)
+        pxG = self.px_H0G3D(event_data)
+        pxnG = self.px_H0nG(event_data)
+
+        likelihood_prb = pG*pxG + pnG*pxnG
+            
+        return likelihood_prb
