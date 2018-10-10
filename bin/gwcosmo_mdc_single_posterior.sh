aa178a039b38daae237b56602e2a72676c33653b
Ignacio <ignacio.magana@me.com>
2018-10-10 04:05:45 -0700

    updated sub file for mdc

--- bin/gwcosmo_mdc_single_posterior.sh
+++ bin/gwcosmo_mdc_single_posterior.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 exec python3 /home/ignacio.magana/src/gwcosmo/bin/gwcosmo_single_posterior \
+--method statistical \
 --posterior_samples /home/ignacio.magana/src/gwcosmo/gwcosmo/data/posterior_samples/first2years-data/2016/lalinference_mcmc/${Process}/posterior_samples.hdf5 \
+--galaxy_catalog_default mdc \
+--mdc_version '2.1' \
 --plot True \
 --save True \
 --outputfile event_${Process} \
---prblikelihood False \
