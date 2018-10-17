1feb779ad77e94a6bc9b7becd590ac9edc60d6a5
Ignacio <ignacio.magana@me.com>
2018-10-17 05:22:00 -0700

    added mdc counterparts script to bin

--- bin/gwcosmo_mdc_single_posterior.sh
+++ bin/gwcosmo_mdc_single_posterior.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec python3 /home/ignacio.magana/src/gwcosmo/bin/gwcosmo_single_posterior \
+exec python3 /home/ignacio.magana/src/gwcosmo/bin/gwcosmo_single_posterior.py \
 --method statistical \
 --posterior_samples /home/ignacio.magana/src/gwcosmo/gwcosmo/data/posterior_samples/first2years-data/2016/lalinference_mcmc/${Process}/posterior_samples.hdf5 \
 --galaxy_catalog_default mdc \
