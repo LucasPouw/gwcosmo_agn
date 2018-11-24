a6cae9c3eb56433663ef11b1a6bae4841822e118
Ignacio Magana <ignacio.magana@ligo.org>
2018-11-24 05:46:21 -0600

    moved unused/obsolete code, fixed pdet functions, cleaned things up

--- gwcosmo/likelihood/detection_probability.py
+++ gwcosmo/likelihood/detection_probability.py
@@ -103,8 +103,7 @@ class DetectionProbability(object):
         if Nside != None:
             self.interp_map = self.__pD_dlradec(self.Nside,self.dl_array)
 
-    
-    
+
     def mchirp(self,m1,m2):
         """
         Calculates the source chirp mass
@@ -333,8 +332,7 @@ class DetectionProbability(object):
     
         return 4.0*A**2*num*np.power(lal.G_SI,5.0/3.0)/lal.C_SI**3.0
 
-        
-        
+
     def __pD_zH0(self,H0):
         """
         Detection probability over a range of distances, returned as an interpolated function.
@@ -380,7 +378,6 @@ class DetectionProbability(object):
         #path = pkg_resources.resource_filename('gwcosmo', 'likelihood/z_H0_pD_array.p')
         #pickle.dump((self.z_array,H0vec,prob),open(path,'wb'))
         return (self.z_array,H0vec,prob)
-        
         
 
     def pD_dlH0_eval(self,dl,H0):
@@ -431,91 +428,43 @@ class DetectionProbability(object):
         pass
 
 
+    def __call__(self, dl, H0):
+        """
+        To call as function of dl and H0
+        
+        Parameters
+        ----------
+        dl : float or array_like
+            value(s) of luminosity distances in Mpc
+        H0 : float
+            H0 value in kms-1Mpc-1
+        
+        Returns
+        -------
+        float or array_like
+            Returns Pdet(dl,H0).
+        """
+        return self.pD_dlH0_eval(dl, H0)
+    
+    
+    def pD_distmax(self, dl, H0):
+        """
+        Parameters
+        ----------
+        dl : float or array_like
+            value(s) of luminosity distances in Mpc
+        H0 : float
+            H0 value in kms-1Mpc-1
+        
+        Returns
+        -------
+        float or array_like
+            Returns twice the maximum distance given corresponding to Pdet(dl,H0) = 0.01.
+        """
+        return 2.*dl[np.where(self.pD_dlH0_eval(dl, H0)>0.01)[0][-1]]
 
 
-
-    ###### PLEASE NOTE FUNCTIONS BEYOND THIS POINT ARE OBSOLETE OR NEED REPAIR ######
-    # TODO: remove obsolete functions
     # TODO: repair pixel-based functions
-    
-    
-    
-        
-        
-    def __pD_zH0_interp(self,H0vec):
-        """
-        OBSOLETE
-        Function which calculates p(D|z,H0) for a range of redshift and H0 values
-        
-        Parameters
-        ----------
-        H0vec : array_like
-            numpy array of H0 values in kms-1Mpc-1
-        
-        Returns
-        -------
-        2D interpolation object over z and H0
-        """
-        # TODO pickle z,H0,prob without interp, and try different interp options
-        prob = np.array([self.__pD_zH0(H0) for H0 in H0vec])
-        interp = interp2d(self.z_array,H0vec,prob)
-        interp_av_path = pkg_resources.resource_filename('gwcosmo', 'likelihood/pD_zH0_interp.p')
-        pickle.dump(interp,open(interp_av_path,'wb'))
-        return interp
-
-    
-    def __snr_squared_old(self,RA,Dec,m1,m2,inc,psi,detector,gmst,z=0):
-        """
-        OBSOLETE
-        the optimal snr squared for one detector, used for marginalising over sky location, inclination, polarisation, mass
-        
-        Parameters
-        ----------
-        RA,Dec : sky location of the event in radians
-        m1,m2 : source masses in kg
-        inc : source inclination in radians
-        psi : source polarisation in radians
-        detector : name of detector in network as a string (eg 'H1', 'L1')
-        gmst : Greenwich Mean Sidereal Time in seconds
-        
-        Returns
-        -------
-        snr squared*dL^2 for given parameters at a single detector
-        """
-        mtot = self.__mtot_obs(m1,m2,z)
-        mc = self.mchirp_obs(m1,m2,z)
-        A = self.__reduced_amplitude(RA,Dec,inc,psi,detector,gmst) * np.power(mc,5.0/6.0)
-        
-        fmax = self.__fmax(mtot)
-        num = self.__interpolnum(fmax)
-    
-        return 4.0*A**2*num*np.power(lal.G_SI,5.0/3.0)/lal.C_SI**3.0
-
-        
-        
-    def __pD_dl_old(self,dl_array):
-        """
-        OBSOLETE
-        Detection probability over a range of distances, returned as an interpolated function.
-        """
-        rho = np.zeros((self.Nsamps,1))
-        for n in range(self.Nsamps):
-            rhosqs = [ self.__snr_squared_old(self.RAs[n],self.Decs[n],self.m1[n],self.m2[n],self.incs[n],self.psis[n], det, 0.0) for det in self.__lal_detectors]
-            rho[n] = np.sum(rhosqs)
-
-        DLcopy = dl_array.reshape((dl_array.size, 1))
-        DLcopy = DLcopy.transpose()
-        DLcopy = 1/(DLcopy*lal.PC_SI*1e6)**2
-
-        effective_threshold = np.sqrt(len(self.detectors)) * self.snr_threshold
-        survival = np.matmul(rho, DLcopy)
-        survival = ncx2.sf(effective_threshold**2,4,survival)
-
-        prob = np.sum(survival,0)/self.Nsamps
-        self.spl = splrep(dl_array,prob)
-        return splrep(dl_array,prob)
-        
-    
     def __pD_dlradec(self,Nside,dl_array):
         """
         NEEDS FIXING
@@ -546,73 +495,8 @@ class DetectionProbability(object):
             pofd_dLRADec.append(splrep(dl_array,norm))
 
         return pofd_dLRADec
-        
-
-    def snr_squared_single(self,DL,RA,Dec,m1,m2,inc,psi,detector,gmst):
-        """
-        UNUSED
-        the optimal snr squared for one detector, for a specific DL, RA, Dec, m1, m2, inc, psi, gmst
-        """
-        mtot = m1+m2
-        mc = self.mchirp_obs(m1,m2)
-        Fplus,Fcross = lal.ComputeDetAMResponse(detector.response, RA, Dec, psi, gmst)
-        A = self.__reduced_amplitude(RA,Dec,inc,psi,detector,gmst) * np.power(mc,5.0/6.0) / (DL*lal.PC_SI*1e6)
-        
-        PSD = self.psds
-        def I(f):
-            return np.power(f,-7.0/3.0)/(PSD(f)**2)
-
-        fmin = 10 # Hz
-        fmax = self.__fmax(mtot)
-        num = quad(I,fmin,fmax,epsabs=0,epsrel=1.49e-4)[0]
-    
-        return 4.0*A**2*num*np.power(lal.G_SI,5.0/3.0)/lal.C_SI**3.0
 
 
-    def pD_event(self, dl, ra, dec, m1, m2, inc, psi, gmst):
-        """
-        UNUSED
-        detection probability for a particular event (masses, distance, sky position, orientation and time)
-        """
-        rhosqs = [ self.snr_squared_single(dl, ra, dec, m1, m2, inc, psi, det, gmst) for det in self.__lal_detectors]
-        combined_rhosq = np.sum(rhosqs)
-        effective_threshold = np.sqrt(len(self.detectors)) * self.snr_threshold
-        return ncx2.sf(effective_threshold**2 , 4, combined_rhosq)
-        
-
-    def pD_dl_single(self, dl):
-        """
-        OBSOLETE
-        Detection probability for a specific distance, averaged over all other parameters - without using interpolation
-        """       
-        return np.mean(
-            [ self.pD_event(dl, self.RAs[i], self.Decs[i], self.m1[i], self.m2[i], self.incs[i], self.psis[i], 0.0) for i in range(N)]
-            )
-            
-
-    def pD_dl_eval_old(self,dl):
-        """
-        OBSOLETE
-        Returns a probability for a given distance dl from the interpolated function.
-        Or an array of probabilities for an array of distances.
-        """
-        return splev(dl,self.interp_average,ext=3)
-        
-        
-    def pD_dlradec_eval(self,dl,RA,Dec,gmst):
-        """
-        OBSOLETE?
-        detection probability evaluated at a specific dl,ra,dec and gmst.
-        """
-        if all(self.interp_map) == None:
-            self.interp_map = self.__pD_dlradec(self.Nside,self.dl_array)
-        no_pix = hp.pixelfunc.nside2npix(self.Nside)
-        pix_ind = range(0,no_pix)
-        survival_func_sky = self.interp_map
-        hpxmap = survival_func_sky(dl)[pix_ind]
-        return hp.get_interp_val(hpxmap,np.pi/2.0-Dec,RA-gmst)
-        
-        
     def pDdl_radec(self,RA,Dec,gmst):
         """
         NEEDS FIXING
@@ -620,21 +504,4 @@ class DetectionProbability(object):
         """
         ipix = hp.ang2pix(self.Nside,np.pi/2.0-Dec,RA-gmst)
         return self.interp_map[ipix]
-        
-        
-    def __call__(self, dl):
-        """
-        NEEDS FIXING
-        To call as function of dl
-        """
-        return self.pD_dl_eval(dl)
     
-    
-    def pD_distmax(self):
-        """
-        NEEDS FIXING
-        Returns twice the maximum distance given Pdet(dl) = 0.01.
-        """
-        return 2.*self.dl_array[np.where(self.pD_dl_eval(self.dl_array)>0.01)[0][-1]]
-        
-
