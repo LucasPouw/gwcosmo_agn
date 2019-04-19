851430c7ea0c76f70aa395c446b8d8b8e6f300ac
Ignacio Magana <ignacio.magana@ligo.org>
2019-04-19 00:57:55 -0500

    load gwtc1 samples

--- gwcosmo/likelihood/posterior_samples.py
+++ gwcosmo/likelihood/posterior_samples.py
@@ -35,19 +35,34 @@ class posterior_samples(object):
             self.latitude = lalinference_data['dec']
             self.weight = np.ones(len(self.latitude))/(self.distance**2 * np.cos(self.latitude))
             self.nsamples = len(self.weight)
-
+        
+        #TODO: handle this better
         if samples_file_path[-4:] == 'hdf5':
-            group_name = 'lalinference_mcmc'
-            dataset_name = 'posterior_samples'
-            f1 = h5py.File(samples_file_path, 'r')
-            group = f1[group_name]
-            lalinference_data = group[dataset_name]
-            self.distance = lalinference_data['dist']
-            self.longitude = lalinference_data['ra']
-            self.latitude = lalinference_data['dec']
-            self.weight = np.ones(len(self.latitude))/(self.distance**2 * np.cos(self.latitude))
-            self.nsamples = len(self.weight)
-            f1.close()
+            if samples_file_path[-11:] == 'GWTC-1.hdf5':
+                if samples_file_path[-20:] == 'GW170817_GWTC-1.hdf5':
+                    dataset_name = 'IMRPhenomPv2NRT_lowSpin_posterior'
+                else:
+                    dataset_name = 'Overall_posterior'
+                f1 = h5py.File(samples_file_path, 'r')
+                lalinference_data = f1[dataset_name]
+                self.distance = lalinference_data['luminosity_distance_Mpc']
+                self.longitude = lalinference_data['right_ascension']
+                self.latitude = lalinference_data['declination']
+                self.weight = np.ones(len(self.latitude))/(self.distance**2 * np.cos(self.latitude))
+                self.nsamples = len(self.weight)
+                f1.close()
+            else:    
+                group_name = 'lalinference_mcmc'
+                dataset_name = 'posterior_samples'
+                f1 = h5py.File(samples_file_path, 'r')
+                group = f1[group_name]
+                lalinference_data = group[dataset_name]
+                self.distance = lalinference_data['dist']
+                self.longitude = lalinference_data['ra']
+                self.latitude = lalinference_data['dec']
+                self.weight = np.ones(len(self.latitude))/(self.distance**2 * np.cos(self.latitude))
+                self.nsamples = len(self.weight)
+                f1.close()
 
         if samples_file_path[-3:] == 'hdf':
             fp = h5py.File(samples_file_path, 'r')
